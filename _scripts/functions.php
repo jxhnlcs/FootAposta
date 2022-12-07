5d42030d5f3c0dbc3ac6314c315f7c1f8eb3df86
John Lucas <joohnzay@gmail.com>
2022-12-06 22:17:02 -0300

    Banco de dados meio funcional

--- _scripts/functions.php
+++ _scripts/functions.php
@@ -1,3 +1,21 @@
 <?php
+function login($email){
+    include "config.php";
+    $sql = "SELECT login FROM login WHERE email = '$email'";
+    $query = $mysqli->query($sql);
+    $dados = $query->fetch_array();
+
+    return $dados['login'];
+}
+
+function validarUsuario($id,$tabela){
+    include "config.php";
+    $sql = "SELECT id FROM $tabela WHERE email = '$id'";
+    $query = $mysqli->query($sql);
+    $total = $query->num_rows;
+
+    return $total;
+
+}
 
 ?>
--- login.php
+++ login.php
@@ -18,11 +18,11 @@
                 <h1>FootAposta</h1>
                 <div class="textfield">
                     <label for="usuario">E-mail</label>
-                    <input type="text" required name="usuario" placeholder="Digite seu e-mail">
+                    <input type="text" required name="email" id='email' placeholder="Digite seu e-mail">
                 </div>
                 <div class="textfield">
                     <label for="senha">Senha</label>
-                    <input type="password" required name="senha" placeholder="Digite sua senha">
+                    <input type="password" required name="senha" id='senha' placeholder="Digite sua senha">
                 </div>
                 <button class="btn-login">ENTRAR</button>
                 <h6 class="linha"> </h6>
@@ -32,3 +32,46 @@
     </div>
 </body>
 </html>
+<?php
+if(!empty($_POST['email'])){
+    include "_scripts/config.php";
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
+
+    if(validarUsuario($senha,'login')>=1){ ?>
+        <script type="text/javascript">
+            Swal.fire(
+            'Ops!',
+            'Verifique se sua senha ou seu e-mail está correto.',
+            'question'
+            )
+        </script>
+    <?php }else{
+
+        $sql = "INSERT INTO login (email,senha) VALUES ('$email','$senha')";
+        $query = $mysqli->query($sql);
+
+        if($query){ ?>
+            <script type="text/javascript">
+                Swal.fire({
+                    title:'Salvo',
+                    text:'Aluno Cadastrado com Sucesso',
+                    icon:'success',
+                    confirmButtonText:'Ok'
+                }).then((result)=>{
+                    if(result.isConfirmed){
+                        location.href="login.php";
+                    }
+                })
+            </script>
+        <?php }else{ ?>  
+            <script type="text/javascript">
+                swal("Erro");
+            </script>
+        <?php }
+    }
+
+
+}
+
+?>
